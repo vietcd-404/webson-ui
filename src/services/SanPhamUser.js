6dac789b1b1d742bf1ca885d2e392fc925d7bf25
Việt Cao <vietcd245@gmail.com>
2023-11-08 01:57:39 +0700

    update hiện sản phẩm trên user

--- src/services/SanPhamUser.js
+++ src/services/SanPhamUser.js
@@ -1,5 +1,5 @@
 import api from "./api";
 
 export const getAll = async (page, size) => {
-  return await api.get(`/user/san-pham/get-all?page=${page}&size=${size}`);
+  return await api.get(`/guest/san-pham/get-all?page=${page}&size=${size}`);
 };
