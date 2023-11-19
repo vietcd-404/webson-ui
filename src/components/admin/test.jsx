bfe185644e56790af718c3fdfac5799e6d612f6b
Việt Cao <vietcd245@gmail.com>
2023-11-19 15:46:34 +0700

    update

--- src/components/admin/test.jsx
+++ src/components/admin/test.jsx
@@ -1,4 +1,4 @@
-import { Card, Form, Select, Space, Table, Button } from "antd";
+import { Card, Form, Select, Space, Table, Button, Input } from "antd";
 import React, { useEffect, useState } from "react";
 import { findAllMau } from "../../services/MauService";
 import { findAllSanPham } from "../../services/SanPhamService";
@@ -7,54 +7,49 @@ import { findAllThuongHieu } from "../../services/ThuongHieuService";
 import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 
 const QLNguoiDung = () => {
-  const [dataMau, setDataMau] = useState([]);
-  const [dataThuongHieu, setDataThuongHieu] = useState([]);
+  const [selectedValues1, setSelectedValues1] = useState([]);
+  const [selectedValues2, setSelectedValues2] = useState([]);
+  const [tableData, setTableData] = useState([]);
+  const [editingKey, setEditingKey] = useState("");
   const [dataLoai, setDataLoai] = useState([]);
   const [dataSanPham, setDataSanPham] = useState([]);
-  const [tableData, setTableData] = useState([]); // Table data
 
-  const [selectedSanPham, setSelectedSanPham] = useState(null);
-  const [selectedLoai, setSelectedLoai] = useState("");
-  const [selectedMau, setSelectedMau] = useState([]);
-  const [selectedThuongHieu, setSelectedThuongHieu] = useState("");
-  const [listCT, setListCT] = useState([]);
+  const [loadingLoai, setLoadingLoai] = useState(false);
 
-  useEffect(() => {
-    loadSanPham();
-    loadMau();
-    loadLoai();
-    loadThuongHieu();
-  }, []);
+  const isEditing = (record) => record.id === editingKey;
 
-  useEffect(() => {
-    updateTableData();
-  }, [selectedSanPham, selectedLoai, selectedMau, selectedThuongHieu]);
-
-  const updateTableData = () => {
-    // Implement your logic to fetch or filter data based on selected values
-    // Replace this logic with your data fetching or filtering code
-    const filteredData = dataSanPham.map((item) => {
-      return (
-        (selectedSanPham === null || item.tenSanPham === selectedSanPham) &&
-        (selectedLoai === "" || item.tenLoai === selectedLoai) &&
-        (selectedThuongHieu === "" ||
-          item.tenThuongHieu === selectedThuongHieu) &&
-        (selectedMau.length === 0 || selectedMau.includes(item.tenMau))
-      );
-    });
-    setTableData(filteredData);
+  const handleSelectChange1 = (values) => {
+    setSelectedValues1(values);
+    updateTableData(selectedValues2, values);
   };
 
-  const loadMau = async () => {
+  const handleSelectChange2 = (values) => {
+    setSelectedValues2(values);
+    updateTableData(values, selectedValues1);
+  };
+
+  const updateTableData = (values1, values2) => {
+    const newData = values1.map((value, index) => ({
+      id: new Date().getTime() + index,
+      value1: value,
+      value2: values2,
+      value3: "Ô 3",
+    }));
+    setTableData(newData);
+  };
+
+  const loadLoaiData = async () => {
+    setLoadingLoai(true);
     try {
-      const response = await findAllMau();
-      setDataMau(response.data);
+      const response = await findAllLoai();
+      setDataLoai(response.data);
     } catch (error) {
       console.error("Lỗi khi gọi API: ", error);
+    } finally {
+      setLoadingLoai(false);
     }
   };
-
-  const loadSanPham = async () => {
+  const loadTableSanPham = async () => {
     try {
       const response = await findAllSanPham();
       setDataSanPham(response.data);
@@ -63,171 +58,105 @@ const QLNguoiDung = () => {
     }
   };
 
-  const loadLoai = async () => {
-    try {
-      const response = await findAllLoai();
-      setDataLoai(response.data);
-    } catch (error) {
-      console.error("Lỗi khi gọi API: ", error);
-    }
-  };
-
-  const loadThuongHieu = async () => {
-    try {
-      const response = await findAllThuongHieu();
-      setDataThuongHieu(response.data);
-    } catch (error) {
-      console.error("Lỗi khi gọi API: ", error);
-    }
-  };
+  useEffect(() => {
+    loadLoaiData();
+    loadTableSanPham();
+  }, []);
 
   const columns = [
     {
-      title: "Tên sản phẩm",
-      dataIndex: "tenSanPham",
-      key: "tenSanPham",
+      title: "Ô 1",
+      dataIndex: "value1",
+      key: "value1",
     },
     {
-      title: "Tên loại",
-      dataIndex: "tenLoai",
-      key: "tenLoai",
+      title: "Ô 2",
+      dataIndex: "value2",
+      key: "value2",
     },
     {
-      title: "Tên thương hiệu",
-      dataIndex: "tenThuongHieu",
-      key: "tenThuongHieu",
+      title: "Ô 3",
+      dataIndex: "value3",
+      key: "value3",
+      render: (text, record) => {
+        const editable = isEditing(record);
+
+        return editable ? (
+          <Input
+            style={{ width: "100px" }}
+            value={text}
+            onChange={(e) => handleTableEdit(e.target.value, "value3", record)}
+            onBlur={() => save(record.id)}
+          />
+        ) : (
+          <div onClick={() => edit(record.id)}>{text}</div>
+        );
+      },
     },
-    {
-      title: "Tên màu",
-      dataIndex: "tenMau",
-      key: "tenMau",
-    },
-    {
-      title: "Giá bán",
-      dataIndex: "giaBan",
-      key: "giaBan",
-    },
-    {
-      title: "Số lượng tồn",
-      dataIndex: "soLuongTon",
-      key: "soLuongTon",
-    },
-    {
-      title: "Phần trăm giảm",
-      dataIndex: "phanTramGiam",
-      key: "phanTramGiam",
-    },
-    {
-      title: "Danh sách ảnh",
-      dataIndex: "phanTramGiam",
-      key: "phanTramGiam",
-    },
-    {
-      title: "Chức năng",
-      key: "action",
-      render: (_, record) => (
-        <Space size="middle">
-          <Button>
-            <EditOutlined />
-          </Button>
-          <Button>
-            <DeleteOutlined />
-          </Button>
-        </Space>
-      ),
-    },
-    // Add more columns for other attributes here
   ];
+
+  const handleTableEdit = (value, dataIndex, record) => {
+    const newTableData = [...tableData];
+    const targetIndex = newTableData.findIndex((item) => record.id === item.id);
+
+    if (targetIndex > -1) {
+      newTableData[targetIndex][dataIndex] = value;
+      setTableData(newTableData);
+    }
+  };
+
+  const edit = (key) => {
+    setEditingKey(key);
+  };
+
+  const save = (key) => {
+    setEditingKey("");
+  };
 
   return (
     <div>
-      <Card title="Tên sản phẩm" bordered={false} style={{ width: "100%" }}>
-        <Select
-          placeholder="Chọn sản phẩm"
-          size="large"
-          style={{
-            width: "100%",
-          }}
-          onChange={(value) => setSelectedSanPham(value)}
-          value={selectedSanPham}
-        >
-          {dataSanPham.map((product) => (
-            <Select.Option key={product.maSanPham} value={product.tenSanPham}>
-              {product.tenSanPham}
-            </Select.Option>
-          ))}
-        </Select>
-      </Card>
-
       <Card
-        title="Thuộc tính"
-        bordered={false}
-        style={{
-          width: "100%",
-        }}
-      >
-        <Form.Item name="loai" label="Loại">
-          <Select
-            mode="multiple"
-            size="large"
-            placeholder="Chọn loại"
-            onChange={(value) => setSelectedLoai(value)}
-            value={selectedLoai}
-            style={{
-              width: "100%",
-            }}
-          >
-            {dataLoai.map((loai) => (
-              <Select.Option key={loai.maLoai} value={loai.tenLoai}>
-                {loai.tenLoai}
-              </Select.Option>
-            ))}
-          </Select>
-        </Form.Item>
-        <Form.Item name="thuongHieu" label="Thương hiệu">
-          <Select
-            placeholder="Chọn thương hiệu"
-            size="large"
-            value={selectedThuongHieu}
-            onChange={(value) => setSelectedThuongHieu(value)}
-            style={{ width: "100%" }}
-          >
-            {dataThuongHieu.map((thuongHieu) => (
-              <Select.Option
-                key={thuongHieu.maThuongHieu}
-                value={thuongHieu.tenThuongHieu}
-              >
-                {thuongHieu.tenThuongHieu}
-              </Select.Option>
-            ))}
-          </Select>
-        </Form.Item>
-        <Form.Item name="mau" label="Màu">
-          <Select
-            mode="multiple"
-            size="large"
-            placeholder="Chọn màu"
-            onChange={(value) => setSelectedMau(value)}
-            value={selectedMau}
-            style={{
-              width: "100%",
-            }}
-          >
-            {dataMau.map((mau) => (
-              <Select.Option key={mau.maMau} value={mau.tenMau}>
-                {mau.tenMau}
-              </Select.Option>
-            ))}
-          </Select>
-        </Form.Item>
-      </Card>
-      <Card
-        title="Danh sách sản phẩm"
+        title="Multi-Selection Fields"
         bordered={false}
         style={{ width: "100%" }}
       >
-        <Table dataSource={tableData} columns={columns} rowKey="maSanPham" />
+        <Select
+          placeholder="Select one or more values for Field 1"
+          value={selectedValues1}
+          onChange={handleSelectChange1}
+          style={{ width: "100%" }}
+        >
+          {dataSanPham.map((item) => (
+            <Select.Option key={item.maSanPham} value={item.tenSanPham}>
+              {item.tenSanPham}
+            </Select.Option>
+          ))}
+        </Select>
+        <Select
+          placeholder="Select one or more values for Field 2"
+          value={selectedValues2}
+          onChange={handleSelectChange2}
+          style={{ width: "100%" }}
+          mode="multiple"
+        >
+          {loadingLoai ? (
+            <Select.Option disabled value="Loading">
+              Loading Loai...
+            </Select.Option>
+          ) : (
+            dataLoai.map((item) => (
+              <Select.Option key={item.maLoai} value={item.tenLoai}>
+                {item.tenLoai}
+              </Select.Option>
+            ))
+          )}
+        </Select>
       </Card>
+
+      <Card title="Table" bordered={false} style={{ width: "100%" }}>
+        <Table dataSource={tableData} columns={columns} rowKey="id" />
+      </Card>
+      <Button>Tạo mới</Button>
     </div>
   );
 };
