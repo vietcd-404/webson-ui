93c217ac4da2ec5c94d658278511809c8c3a6ef0
embearv1 <nguyenvanem2k3@gmail.com>
2023-11-22 17:22:20 +0700

    fix login

--- src/pages/customer/Account/SignIn.js
+++ src/pages/customer/Account/SignIn.js
@@ -45,83 +45,57 @@ const SignIn = () => {
   //   e.preventDefault();
   // };
   return (
-    <div className="container mx-auto h-screen flex items-center justify-center">
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <ToastContainer />
-      <div className="w-full lgl:w-1/2 h-full">
-        {successMsg ? (
-          <div className="w-full lgl:w-[200px] h-full flex flex-col justify-center">
-            <p className="w-full px-4 py-10 text-green-500 font-medium font-titleFont">
-              <ToastContainer />
+      <div className="w-full sm:max-w-md">
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <h1 className="font-semibold text-3xl mb-4 text-center">Đăng Nhập</h1>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-semibold mb-2">
+              Tên đăng nhập
+            </label>
+            <input
+              onChange={handleEmail}
+              value={username}
+              className="w-full h-10 px-4 rounded border border-gray-450 focus:outline-none focus:border-blue-500"
+              type="text"
+              placeholder="Tên đăng nhập"
+            />
+            {errEmail && (
+              <p className="text-red-500 text-xs italic">{errEmail}</p>
+            )}
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-700 text-sm font-semibold mb-2">
+              Mật khẩu
+            </label>
+            <input
+              onChange={handlePassword}
+              value={password}
+              className="w-full h-10 px-4 rounded border border-gray-450 focus:outline-none focus:border-blue-500"
+              type="password"
+              placeholder="Mật khẩu"
+            />
+            {errPassword && (
+              <p className="text-red-500 text-xs italic">{errPassword}</p>
+            )}
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              onClick={handleSignUp}
+              className="bg-red-500 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Đăng nhập
+            </button>
+            <p className="text-sm">
+              Đăng ký thành viên?{" "}
+              <Link to="/signup" className="text-blue-500 hover:text-blue-700">
+                Đăng ký
+              </Link>
             </p>
           </div>
-        ) : (
-          <form
-            // onSubmit={handleSubmit}
-            className="w-full lgl:w-[450px] h-screen flex items-center justify-center"
-          >
-            <div className="px-6 py-4 w-full h-[90%] flex flex-col justify-center ">
-              <h1 className="font-titleFont underline underline-offset-4 decoration-[1px] font-semibold text-3xl mdl:text-4xl mb-4">
-                Đăng Nhập
-              </h1>
-              <div className="flex flex-col gap-3">
-                {/* Email */}
-                <div className="flex flex-col gap-.5">
-                  <p className="font-titleFont text-base font-semibold text-gray-600">
-                    Tên đăng nhập
-                  </p>
-                  <input
-                    onChange={handleEmail}
-                    value={username}
-                    className="w-full h-8 placeholder:text-sm placeholder:tracking-wide px-4 text-base font-medium placeholder:font-normal rounded-md border-[1px] border-gray-400 outline-none"
-                    type="text"
-                    placeholder="username"
-                  />
-                  {errEmail && (
-                    <p className="text-sm text-red-500 font-titleFont font-semibold px-4">
-                      <span className="font-bold italic mr-1">!</span>
-                      {errEmail}
-                    </p>
-                  )}
-                </div>
-
-                {/* Password */}
-                <div className="flex flex-col gap-.5">
-                  <p className="font-titleFont text-base font-semibold text-gray-600">
-                    Mật khẩu
-                  </p>
-                  <input
-                    onChange={handlePassword}
-                    value={password}
-                    className="w-full h-8 placeholder:text-sm placeholder:tracking-wide px-4 text-base font-medium placeholder:font-normal rounded-md border-[1px] border-gray-400 outline-none"
-                    type="password"
-                    placeholder="password"
-                  />
-                  {errPassword && (
-                    <p className="text-sm text-red-500 font-titleFont font-semibold px-4">
-                      <span className="font-bold italic mr-1">!</span>
-                      {errPassword}
-                    </p>
-                  )}
-                </div>
-
-                <button
-                  onClick={handleSignUp}
-                  className=" text-white bg-[#cc0000] text-gray-200 hover:text-white cursor-pointer w-full text-base font-medium h-10 rounded-md  duration-300"
-                >
-                  Đăng nhập
-                </button>
-                <p className="text-sm text-center font-titleFont font-medium">
-                  Đăng ký thành viên?{" "}
-                  <Link to="/signup">
-                    <span className="hover:text-blue-600 duration-300 text-red-600">
-                      Đăng ký
-                    </span>
-                  </Link>
-                </p>
-              </div>
-            </div>
-          </form>
-        )}
+        </form>
       </div>
     </div>
   );
