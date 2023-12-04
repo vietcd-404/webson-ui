7aa26a8e60f3964fb1850f7876c504a9e5bf3e02
Việt Cao <vietcd245@gmail.com>
2023-12-04 19:37:07 +0700

    update bán hàng tại quầy

--- src/components/admin/BanHangTaiQuay/BanHang2.jsx
+++ src/components/admin/BanHangTaiQuay/BanHang2.jsx
@@ -15,14 +15,18 @@ import {
 import Input from "antd/es/input/Input";
 import {
   findSanPham,
+  getAllKhachHang,
   getAllTaiQuay,
   hienHoaDonTaiQuay,
   taoHoaDonTaiQuay,
+  thanhToanHoaDon,
   themSanPhamTaiQuay,
   updateSoLuongTaiQuay,
+  xoaSanPhamTaiQuay,
 } from "../../../services/BanHangTaiQuay";
 import { DeleteOutlined } from "@ant-design/icons";
 import { useAuth } from "../../../pages/customer/Account/AuthProvider";
+import Swal from "sweetalert2";
 const HoaDon2 = () => {
   const [activeKey, setActiveKey] = useState("1");
   const [items, setItems] = useState(() => {
@@ -30,23 +34,62 @@ const HoaDon2 = () => {
     return storedItems ? JSON.parse(storedItems) : [];
   });
   const [dataSanPham, setDataSanPham] = useState([]);
+  const [khachHang, setKhachhang] = useState([]);
   const [tongTien, setTongTien] = useState([]);
+  const [selectedTenKhachHang, setSelectedTenKhachHang] = useState(false);
 
   const [dataDaThemSP, setDaThemSP] = useState([]);
   const [selectedSanPham, setSelectedSanPham] = useState(null);
   const [taoHoaDon, setTaoHoaDon] = useState({});
   const [formData, setFormData] = useState({
     tenNguoiNhan: "",
+    maNguoiDung: "",
+    diaChi: "",
+    sdt: "",
   });
+  const [formSP, setFormSP] = useState({
+    trong: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "maNguoiDung" && value === "KhachNgoai") {
+      // Nếu chọn option "Khách ngoài", đặt giá trị của maNguoiDung bằng null
+      setFormData({
+        ...formData,
+        [name]: null,
+      });
+    } else {
+      // Ngược lại, giữ nguyên giá trị của maNguoiDung
+      setFormData({
+        ...formData,
+        [name]: value,
+      });
+    }
+  };
+
+  const [editFormData, setEditFormData] = useState(null); // Data for editing
 
   const newTabIndex = useRef(1);
   const handleSelectSanPham = (values) => {
     setSelectedSanPham("");
   };
+  const handleSelectTenKhachHang = (values) => {
+    setSelectedTenKhachHang(values);
+  };
   const loadSanPham = async () => {
     try {
       const response = await getAllTaiQuay();
       setDataSanPham(response.data.content);
+    } catch (error) {
+      console.error("Lỗi khi gọi API: ", error);
+    }
+  };
+
+  const loadKhachHang = async () => {
+    try {
+      const response = await getAllKhachHang();
+      setKhachhang(response.data);
     } catch (error) {
       console.error("Lỗi khi gọi API: ", error);
     }
@@ -74,6 +117,7 @@ const HoaDon2 = () => {
       //   icon: "success",
       // });
       loadDaThemSanPham(maHoaDon);
+      loadSanPham();
       message.success("Thêm thành công");
       return;
     } catch (error) {
@@ -81,8 +125,20 @@ const HoaDon2 = () => {
       message.error(error.response?.data?.message || "Error adding to cart");
     }
   };
+  const handleThanhToan = async (maHoaDon) => {
+    try {
+      // const maHoaDon = items.find((item) => item.key === maHoaDon)?.maHoaDon;
+      await thanhToanHoaDon(maHoaDon, formData);
+      console.log(maHoaDon);
+      message.success("Thanh toán thành công");
+      return;
+    } catch (error) {
+      console.log("Lỗi ", error);
+    }
+  };
 
   useEffect(() => {
+    loadKhachHang();
     loadSanPham();
     const storedItems = localStorage.getItem("items");
     if (storedItems) {
@@ -170,34 +226,110 @@ const HoaDon2 = () => {
     // newTabIndex.current++;
   };
   const handleQuantityChange = async (
-    newQuantity,
+    event,
     maHoaDonCT,
     maHoaDon,
     maxQuantity
   ) => {
-    // const newQuantity = event.target.value;
-    if (newQuantity > maxQuantity || newQuantity < 1) {
+    const newQuantity = event.target.value; // Ensure it's parsed as an integer
+
+    if (maxQuantity === 0) {
       // You can choose to show an error message or handle it in a way suitable for your application
       console.error("Quantity exceeds the maximum limit");
       message.error("Số lượng vượt giới hạn");
       return;
     }
+    if (newQuantity > 0) {
+      try {
+        await updateSoLuongTaiQuay(maHoaDonCT, newQuantity, maHoaDon);
 
-    try {
-      await updateSoLuongTaiQuay(maHoaDonCT, newQuantity, maHoaDon);
-
-      loadDaThemSanPham(maHoaDon);
-    } catch (error) {
-      console.error("Failed to update quantity:", error);
-      message.error(error.response.data.message);
+        loadSanPham();
+        loadDaThemSanPham(maHoaDon);
+      } catch (error) {
+        console.error("Failed to update quantity:", error);
+        message.error(error.response.data.message);
+      }
+      setDaThemSP((prevData) =>
+        prevData.map((item) =>
+          item.maHoaDonCT === maHoaDonCT
+            ? { ...item, soLuong: newQuantity }
+            : item
+        )
+      );
     }
-    setDaThemSP((prevData) =>
-      prevData.map((item) =>
-        item.maHoaDonCT === maHoaDonCT
-          ? { ...item, soLuong: newQuantity }
-          : item
-      )
-    );
+  };
+
+  // const handleQuantityChange = async (
+  //   event,
+  //   maHoaDonCT,
+  //   maHoaDon,
+  //   maxQuantity,
+  //   action
+  // ) => {
+  //   let newQuantity;
+
+  //   if (action === "increment") {
+  //     newQuantity++;
+  //   } else if (action === "decrement") {
+  //     newQuantity--;
+  //   } else {
+  //     newQuantity = parseInt(event.target.value, 10);
+  //   }
+
+  //   if (maxQuantity + 1 === 0 || newQuantity < 1) {
+  //     // Handle error or show a message
+  //     console.error("Quantity exceeds the maximum limit");
+  //     message.error("Số lượng vượt giới hạn");
+  //     return;
+  //   }
+
+  //   if (maxQuantity > 1) {
+  //     try {
+  //       await updateSoLuongTaiQuay(maHoaDonCT, newQuantity, maHoaDon);
+
+  //       loadSanPham();
+  //       loadDaThemSanPham(maHoaDon);
+  //     } catch (error) {
+  //       console.error("Failed to update quantity:", error);
+  //       message.error(error.response.data.message);
+  //     }
+
+  //     setDaThemSP((prevData) =>
+  //       prevData.map((item) =>
+  //         item.maHoaDonCT === maHoaDonCT
+  //           ? { ...item, soLuong: newQuantity }
+  //           : item
+  //       )
+  //     );
+  //   }
+  // };
+
+  const xoaSanPhamCT = (ma, maHoaDon) => {
+    Swal.fire({
+      title: "Bạn có chắc không?",
+      text: "Bạn có muốn xóa sản phẩm không!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Vâng, tôi muốn!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          Swal.fire({
+            title: "Xóa!",
+            text: "Bạn đã xóa thành công.",
+            icon: "success",
+          });
+          const response = await xoaSanPhamTaiQuay(ma);
+          console.log(response);
+          loadSanPham();
+          loadDaThemSanPham(maHoaDon);
+        } catch (error) {
+          message.error(error.response.data.message);
+        }
+      }
+    });
   };
   console.log();
   const columns = [
@@ -212,19 +344,22 @@ const HoaDon2 = () => {
       dataIndex: "soLuong",
       key: "soLuong",
       render: (text, record) => (
-        <InputNumber
-          min={1}
-          max={record.soLuongTon}
-          defaultValue={text}
-          onChange={(newQuantity) =>
-            handleQuantityChange(
-              newQuantity,
-              record.maHoaDonCT,
-              record.maHoaDon,
-              record.soLuongTon
-            )
-          }
-        />
+        <>
+          <Input
+            min={1}
+            type="number"
+            max={record.soLuongTon === 0 ? record.soLuong : "disabled"}
+            value={record.soLuong}
+            onChange={(e) =>
+              handleQuantityChange(
+                e,
+                record.maHoaDonCT,
+                record.maHoaDon,
+                record.soLuongTon
+              )
+            }
+          />
+        </>
       ),
     },
     {
@@ -247,7 +382,9 @@ const HoaDon2 = () => {
           {/* <Button onClick={() => showEditModal(record)}>
             <EditOutlined />
           </Button> */}
-          <Button>
+          <Button
+            onClick={() => xoaSanPhamCT(record.maHoaDonCT, record.maHoaDon)}
+          >
             <DeleteOutlined />
           </Button>
         </Space>
@@ -321,43 +458,113 @@ const HoaDon2 = () => {
                   marginRight: "10px",
                 }}
               >
-                {/* Left side content */}
-                <Form.Item
-                  label="Người bán"
-                  style={{ width: "80%", marginLeft: "40px" }}
-                  rules={[
-                    {
-                      required: true,
-                      message: "Tên loại không được để trống!",
-                    },
-                  ]}
-                >
-                  <Input value={formData.tenNguoiNhan} placeholder="Tên" />
-                </Form.Item>
-                <Form.Item
-                  label="Người bán"
-                  style={{ width: "80%", marginLeft: "40px" }}
-                  rules={[
-                    {
-                      required: true,
-                      message: "Tên loại không được để trống!",
-                    },
-                  ]}
-                >
-                  <Input value={pane.maHoaDon} placeholder="Tên" />
-                </Form.Item>
-                <Form.Item
-                  label="Người bán"
-                  style={{ width: "80%", marginLeft: "40px" }}
-                  rules={[
-                    {
-                      required: true,
-                      message: "Tên loại không được để trống!",
-                    },
-                  ]}
-                >
-                  <Input value={tongTien} placeholder="Tên" />
-                </Form.Item>
+                <div>
+                  <div className="border rounded-md p-4">
+                    <div className="mb-4">
+                      <label
+                        className="block text-sm font-bold mb-2"
+                        htmlFor="nguoiBan"
+                      >
+                        Người bán
+                      </label>
+                      <input
+                        className="border rounded w-3/4 p-2"
+                        id="nguoiBan"
+                        type="text"
+                        value={formData.tenNguoiNhan}
+                        readOnly
+                      />
+                    </div>
+                    <div className="mb-4">
+                      <label
+                        className="block text-sm font-bold mb-2"
+                        htmlFor="nguoiBan"
+                      >
+                        Hóa đơn
+                      </label>
+                      <input
+                        className="border rounded w-3/4 p-2"
+                        type="text"
+                        value={pane.maHoaDon}
+                      />
+                    </div>
+                    <div className="mb-4">
+                      <label
+                        className="block text-sm font-bold mb-2"
+                        htmlFor="khachHang"
+                      >
+                        Khách hàng
+                      </label>
+                      <select
+                        className="border rounded w-full p-2"
+                        id="khachHang"
+                        name="maNguoiDung"
+                        onChange={handleChange}
+                        // value={formData.maNguoiDung || "KhachNgoai"}
+                      >
+                        <option value="KhachNgoai">Khách ngoài</option>
+                        {khachHang.map((item) => (
+                          <option
+                            key={item.maNguoiDung}
+                            value={item.maNguoiDung}
+                          >
+                            {item.tenKhachHang}
+                          </option>
+                        ))}
+                      </select>
+                    </div>
+                    <div className="mb-4">
+                      <label
+                        className="block text-sm font-bold mb-2"
+                        htmlFor="nguoiBan"
+                      >
+                        Địa chỉ
+                      </label>
+                      <input
+                        name="diaChi"
+                        className="border rounded w-3/4 p-2"
+                        type="text"
+                        onChange={handleChange}
+                      />
+                    </div>
+                    <div className="mb-4">
+                      <label
+                        className="block text-sm font-bold mb-2"
+                        htmlFor="nguoiBan"
+                      >
+                        Sdt
+                      </label>
+                      <input
+                        name="sdt"
+                        className="border rounded w-3/4 p-2"
+                        type="text"
+                        onChange={handleChange}
+                      />
+                    </div>
+                    <div className="mb-4">
+                      <label
+                        className="block text-sm font-bold mb-2"
+                        htmlFor="nguoiBan"
+                      >
+                        Phương thức thanh toán
+                      </label>
+                      <select
+                        className="border rounded w-full p-2"
+                        id="khachHang"
+                        name="maNguoiDung"
+                        onChange={handleChange}
+                      >
+                        <option value="KhachNgoai">Tiền mặt</option>
+                        <option value="KhachNgoai">Chuyển khoản</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="border p-2 rounded-md">
+                    <button onClick={() => handleThanhToan(pane.maHoaDon)}>
+                      Thanh toán
+                    </button>
+                  </div>
+                </div>
               </Col>
 
               <Col
@@ -374,7 +581,7 @@ const HoaDon2 = () => {
                     showSearch
                     size="large"
                     placeholder="Tìm kiếm sản phẩm"
-                    value=""
+                    value={formSP.trong}
                     onSelect={(value, option) =>
                       handleAddToCart(value, pane.maHoaDon)
                     }
