ea456f1d33fb6654e9a0d67dc54e05d66e2221f6
embearv1 <nguyenvanem2k3@gmail.com>
2023-12-11 21:12:06 +0700

    xuat hd#1

--- src/components/admin/XuatHoaDon.jsx
+++ src/components/admin/XuatHoaDon.jsx
@@ -8,11 +8,16 @@ class XuatHoaDon extends React.Component {
 
     html2canvas(input).then((canvas) => {
       const pdf = new jsPDF();
-      const imgProps = pdf.getImageProperties(logo);
-      const pdfWidth = pdf.internal.pageSize.getWidth();
-      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      const imgData = canvas.toDataURL("image/png");
 
-      pdf.addImage(logo, "PNG", 0, 0, pdfWidth, pdfHeight);
+      // Lấy kích thước của trang PDF và tính toán kích thước cho ảnh
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
+
+      // Thêm ảnh từ canvas vào tệp PDF
+      pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
+
+      // Lưu tệp PDF
       pdf.save("invoice.pdf");
     });
   };
@@ -22,138 +27,155 @@ class XuatHoaDon extends React.Component {
       <div>
         <button onClick={this.exportPDF}>Export PDF</button>
         <div id="pdf-content" className="card">
-          <div className="card">
-            <div className="card-header bg-black"></div>
-            <div className="card-body">
-              <div className="container">
-                <div className="row">
-                  <div className="col-xl-12">
-                    <i className="far fa-building text-danger fa-6x float-start"></i>
-                  </div>
+          <div className="card-header bg-black"></div>
+          <div className="card-body">
+            <div className="container">
+              <div className="row">
+                <div className="col-xl-12">
+                  <i className="far fa-building text-danger fa-6x float-start"></i>
                 </div>
+              </div>
 
-                <div className="row">
-                  <div className="col-xl-12">
-                    <img src={logo} alt="Logo" width="80" />
-
-                    <ul className="list-unstyled">
-                      <li style={{ fontSize: "30px", color: "red" }}>
-                        Heva Shop
-                      </li>
-                      <li>Phạm Văn Bạch-Yên Hòa-Cầu Giấy Hà Nội</li>
-                      <li>+84.9313.32203</li>
-                      <li>hevashop@mail.com</li>
-                    </ul>
-                  </div>
+              <div className="row">
+                <div className="col-xl-9">
+                  <img src={logo} alt="Logo" width="80" />
                 </div>
-
-                <div className="row text-center">
-                  <h3
-                    className="text-uppercase text-center mt-3"
-                    style={{ fontSize: "40px" }}
-                  >
-                    Hóa Đơn
-                  </h3>
-                  <p>#HD123456789</p>
+                <div className="col-xl-3">
+                  <ul className="list-unstyled">
+                    <li style={{ fontSize: "30px", color: "red" }}>
+                      Heva Shop
+                    </li>
+                    <li>Địa chỉ: Phạm Văn Bạch-Yên Hòa-Cầu Giấy Hà Nội</li>
+                    <li>Sđt: +84.9313.32203</li>
+                    <li>Email: hevashop@mail.com</li>
+                  </ul>
                 </div>
+              </div>
 
-                <div className="row mx-3">
-                  <table className="table">
-                    <thead>
-                      <tr>
-                        <th scope="col">Sản Phẩm</th>
-                        <th scope="col">Số lượng</th>
-                        <th scope="col">Thành tiền</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>Samsung TV</td>
-                        <td>
-                          <i className="fas fa-dollar-sign"></i> 500,00
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>JBL Speaker</td>
-                        <td>
-                          <i className="fas fa-dollar-sign"></i> 300,00
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>Macbook Air</td>
-                        <td>
-                          <i className="fas fa-dollar-sign"></i> 1000,00
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>Iphone 11 PRO</td>
-                        <td>
-                          <i className="fas fa-dollar-sign"></i> 5000,00
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-
-                <div className="row">
-                  <div className="col-xl-8">
-                    <ul className="list-unstyled float-end me-0">
-                      <li>
-                        <span className="me-3 float-start">Tổng tiền:</span>
-                        <i className="fas fa-dollar-sign"></i> 6850,00
-                      </li>
-                      <li>
-                        <span
-                          className="float-start"
-                          style={{ marginRight: "35px" }}
-                        >
-                          Phí ship:{" "}
-                        </span>
-                        <i className="fas fa-dollar-sign"></i> -500,00
-                      </li>
-                      <li>
-                        <span
-                          className="float-start"
-                          style={{ marginRight: "35px" }}
-                        >
-                          Phí ship:{" "}
-                        </span>
-                        <i className="fas fa-dollar-sign"></i> -500,00
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-
-                <hr />
-
-                <div className="row">
-                  <div className="col-xl-8" style={{ marginLeft: "60px" }}>
-                    <p
-                      className="float-end"
-                      style={{
-                        fontSize: "30px",
-                        color: "red",
-                        fontWeight: 400,
-                        fontFamily: "Arial, Helvetica, sans-serif",
-                      }}
-                    >
-                      Total:{" "}
-                      <span>
-                        <i className="fas fa-dollar-sign"></i> 6350,00
+              <div className="row text-center">
+                <h3
+                  className="text-uppercase text-center mt-3 mb-2"
+                  style={{ fontSize: "40px", fontWeight: "bold" }}
+                >
+                  Hóa Đơn
+                </h3>
+                <p c>#HD</p>
+              </div>
+              <div
+                className="row clear-both mx-3 mt-2"
+                style={{ fontSize: "15px" }}
+              >
+                <div>Ngày mua:</div>
+                <div>Khách hàng:</div>
+                <div>Địa chỉ:</div>
+                <div>Số điện thoại:</div>
+                <div>Địa chỉ:</div>
+                <div>Nhân viên:</div>
+              </div>
+              <div className="row mx-3 mt-3">
+                <p
+                  className="text-center"
+                  style={{ fontSize: "22px", fontWeight: "bold" }}
+                >
+                  Danh sách sản phẩm mua
+                </p>
+              </div>
+              <div className="row mx-3 mt-3">
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th scope="col">Sản Phẩm</th>
+                      <th scope="col">Số lượng</th>
+                      <th scope="col">Đơn giá</th>
+                      <th scope="col">Thành tiền</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div className="row mt-2 mb-4">
+                <div className="col-xl-8">
+                  <ul className="list-unstyled float-end me-0">
+                    <li>
+                      <span
+                        className="float-start"
+                        style={{
+                          marginRight: "20px",
+                          fontWeight: "bold",
+                          fontSize: "17px",
+                        }}
+                      >
+                        Tổng tiền: -
                       </span>
-                    </p>
-                  </div>
+                    </li>
+                    <li>
+                      <span
+                        className="float-start"
+                        style={{
+                          marginRight: "40px",
+                          fontWeight: "bold",
+                          fontSize: "17px",
+                        }}
+                      >
+                        Chiết khấu: %
+                      </span>
+                    </li>
+                    <li>
+                      <span
+                        className="float-start"
+                        style={{
+                          marginRight: "35px",
+                          fontWeight: "bold",
+                          fontSize: "17px",
+                        }}
+                      >
+                        Phí ship: -
+                      </span>
+                    </li>
+                    <li>
+                      <span
+                        className="float-start"
+                        style={{
+                          marginRight: "35px",
+                          fontWeight: "bold",
+                          fontSize: "17px",
+                        }}
+                      >
+                        Trạng thái: -
+                      </span>
+                    </li>
+                  </ul>
                 </div>
+              </div>
 
-                <div className="row mt-2 mb-5">
-                  <p className="fw-bold">
-                    Date: <span className="text-muted">23 June 20221</span>
+              <hr />
+
+              <div className="row">
+                <div className="col-xl-8" style={{ marginLeft: "60px" }}>
+                  <p
+                    className="float-end mt-1"
+                    style={{
+                      fontSize: "30px",
+                      color: "red",
+                      fontWeight: 400,
+                      fontFamily: "Arial, Helvetica, sans-serif",
+                    }}
+                  >
+                    Thành tiền: <span></span>
                   </p>
-                  <p className="fw-bold mt-3">Signature:</p>
                 </div>
               </div>
             </div>
-            <div className="card-footer bg-black"></div>
+          </div>
+          <div className="card-footer bg-black text-[#ffff] text-center font-size-10">
+            Cảm ơn quý khách
           </div>
         </div>
       </div>
